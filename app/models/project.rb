0b1450ca75e86350a412424a10ce42e49cfa18b6
Christoph <mussichhaben@kennstenicht.de>
2018-04-02 12:50:24 +0200

    Fix(ProjectModel): Add comma to translation

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,5 @@ class Project < ApplicationRecord
   extend Mobility
   translates :title,                          type: :string, fallbacks: { de: :en, en: :de }
   translates :subtitle,                       type: :string
-  translates :excerpt, :meta_info, :content   type: :text
+  translates :excerpt, :meta_info, :content,  type: :text
 end
